ccc6f18319fef7d0630ccb8a0bccac7e627837aa
jlongardt <jlongardt@users.noreply.github.com>
2021-01-17 20:52:49 +0100

    First funcionality

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ $(document).ready(function(){
     alert("Works!");
   });
 });
+
+var video = document.querySelector(".videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
