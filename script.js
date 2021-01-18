86bebfdc8767a672aab106d50a42147b80f6b4df
jlongardt <jlongardt@users.noreply.github.com>
2021-01-18 11:34:09 +0100

    Resize of mirror canvas, adding of flags, shadow+glow effects

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log("Something went wrong!");
     });
 }
+
+
+var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
+    radius = '14vw', //distance from center
+    start = -90, //shift start from 0
+    $elements = $('li:not(:first-child)'),
+    numberOfElements = (type === 1) ?  $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+    slice = 360 * type / numberOfElements;
+
+$elements.each(function(i) {
+    var $self = $(this),
+        rotate = slice * i + start,
+        rotateReverse = rotate * -1;
+
+    $self.css({
+        'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+    });
+});
